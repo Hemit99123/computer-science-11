f43eb006dbcf9ef62fe73f9a43ce4bcf12c54b81
Hemit Patel <patelvhemit@gmail.com>
2024-09-24 16:27:22 -0400

    Create Grade Average.py

---
+++ input-out/Grade Average.py
@@ -0,0 +1,19 @@
+# Hemit Patel
+# 111111
+# ICS3U0-1
+# Grade Average Computer Program
+# Mr UNKNOWN 
+# 24 sep 2024
+
+first_grade = int(input("Enter first grade:"));
+second_grade = int(input("Enter second grade:"));
+third_grade = int(input("Enter third grade:"));
+fourth_grade = int(input("Enter fourth grade:"));
+fifth_grade = int(input("Enter fifth grade:"));
+
+
+sum_grades = first_grade + second_grade + third_grade + fourth_grade + fifth_grade
+
+average_grade = sum_grades / 5
+
+print(average_grade)
