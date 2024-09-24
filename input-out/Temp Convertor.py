6eaf66162d1a1c1da3ad063a86ffa98277477581
Hemit Patel <patelvhemit@gmail.com>
2024-09-24 16:41:07 -0400

    Create Temp Convertor.py

---
+++ input-out/Temp Convertor.py
@@ -0,0 +1,12 @@
+# Hemit Patel
+# STUDENT NUMBER
+# ICS3U0-1
+# Temp Converter
+# INSTRUCTOR NAME
+# 24 sep 2024
+
+fahrenheit_temp = int(input("Enter the weather in fahrenheit"));
+
+celisus_temp = 5/9*(fahrenheit_temp - 32);
+
+print("Your weather in Celisus:", celisus_temp)
