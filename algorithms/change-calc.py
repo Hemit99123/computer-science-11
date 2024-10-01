f89bcae9d1f8e68edb3ddfac0fb95d47f413dfaa
Hemit Patel <patelvhemit@gmail.com>
2024-10-01 11:03:02 -0400

    Create change-calc.py

---
+++ algorithms/change-calc.py
@@ -0,0 +1,21 @@
+# Hemit Patel
+# STUDENT NUMBER
+# ICS3U0-4
+# Change Calculator
+# INSTRUTOR NAME
+# 1 oct 2024
+
+
+change = int(input("Eneter the change in cents:"))
+
+quarters = change // 25
+
+remainder = change % 25
+
+dimes = remainder // 10
+
+remainder_2 = remainder % 10
+
+nickels = remainer_2 // 5
+
+pennies = remainder_2 % 5
