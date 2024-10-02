f184d0aecd1264dc315891799be3ff8edea3cc26
Hemit Patel <patelvhemit@gmail.com>
2024-10-02 11:17:13 -0400

    Create day-calc.py

---
+++ algorithms/day-calc.py
@@ -0,0 +1,14 @@
+# Hemit Patel
+# STUDENT NUMBER
+# ICS3U0-4
+# Day into Weeks and leftover Days
+# INSTRUCTOR NAME
+# 2 oct 2024
+
+
+total_day = int(input(""))
+
+weeks = total_day // 7
+days = total_day % 7
+
+print(weeks, days)
