36b92cab6244781c3482a182523b40579b04e999
Hemit Patel <patelvhemit@gmail.com>
2024-10-04 18:13:41 -0400

    Update Project Budget.py

--- input-out/Project Budget.py
+++ input-out/Project Budget.py
@@ -5,17 +5,23 @@
 # INSTRUCTOR NAME
 # 4 oct 2024
 
+# All the inputs needed 
 designing_mins = int(input())
 coding_mins = int(input())
 debugging_mins = int(input())
 testing_mins = int(input())
 
+# Getting the total time from the inputs
 total_time = designing_mins + coding_mins + debugging_mins + testing_mins
 
+# Using that to find out how much percentage each task takes up
 designing_percentage = (designing_mins / total_time) * 100
 coding_percentage = (coding_mins / total_time) * 100
 debugging_percentage = (debugging_mins / total_time) * 100
 testing_percentage = (testing_mins / total_time) * 100
+
+# Outputting a formatted table using the formatted output for string (adds spaces)
+# Also added numerical formatting to the table for rounding 2 decimal places
 
 print("{:<15}{:>10}".format("Task","% Time"))
 print("{:<15}{:>10}".format("Designing","{:.2f} %".format(designing_percentage)))
