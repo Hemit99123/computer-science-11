7fd5cb99a44ab12b6c675c9dd1dcf8abe2a6e018
Hemit Patel <patelvhemit@gmail.com>
2024-10-05 15:06:12 -0400

    Update output-table.py

--- output-table.py
+++ output-table.py
@@ -15,3 +15,16 @@ print("{:^15}{:<10}".format("Task","% Time"))
 
 
 print("{:<15}{:<5.2f}{:2}".format("Designing",percent_design,"%"))
+
+
+
+
+# Formatted with spaces so it resembles a table-like structure
+print("{:<15}{:>10.2f}%".format("Designing", percentage_designing))
+print("{:<15}{:>10.2f}%".format("Coding", percentage_coding))
+print("{:<15}{:>10.2f}%".format("Debugging", percentage_debugging))
+print("{:<15}{:>10.2f}%".format("Testing", percentage_testing))
+
+
+
+## CHANGE THE SPACING DEPENDING ON UR LARGEST TEXT
