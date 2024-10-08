ef805a9b80308e2e0aa92864e12fa9af8d3ff2f8
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-10-07 21:38:14 -0400

    more cataloging

--- output-formatting/scientific-notation.py
+++ output-formatting/scientific-notation.py
@@ -2,4 +2,6 @@ energy = 100000000000
 amount_100wat = 1000000000000
 
 print("The energy produced in joules is = {:.1E}".format(energy))
-print("The number of 100 watt lightbulb powered is = {:.1E}".format(amount_100watt))
+
+
+print("The number of 100 watt lightbulb powered is = {:.1e}".format(amount_100watt))
