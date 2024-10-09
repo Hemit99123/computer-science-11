5fb36eb98c0cd1c96f6dae1469d04b6845aad754
Hemit Patel <patelvhemit@gmail.com>
2024-10-09 11:08:10 -0400

    Create wave-surfer.py

---
+++ wave-surfer.py
@@ -0,0 +1,18 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# Wave surfer conditonal logic
+# MR VEERA
+# 9 oct 2024
+
+
+wave_height = int(input("Enter the amount of wave height:"))
+
+# If wave is higher than 6 feet print an output
+
+if (wave_height >= 6):
+    print("Great day for surfing!")
+
+# If this condition isn't meant, waves are under 6 feet which means another output 
+else:
+    print("Go body boarding!")
