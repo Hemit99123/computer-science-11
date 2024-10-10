2b936d8da2e2dfcae715556cbb12342b37df5e60
Hemit Patel <patelvhemit@gmail.com>
2024-10-10 10:25:15 -0400

    Create LEARN BETTER (RANGES).py

---
+++ LEARN BETTER (RANGES).py
@@ -0,0 +1,21 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# Surfers Application
+# MR VEERA
+# 10 oct 2024
+
+waves = int(input("Enter the amount of waves:"))
+
+if (waves > 6):
+    print("Great day for surfing")
+    
+elif (waves >= 3 and waves < 6):
+    print("Go body boarding!")
+
+elif (waves >= 0 and waves < 3):
+    print("Go for a swim")
+
+
+else:
+    print("Whoa! What kind of a surf is that?")
