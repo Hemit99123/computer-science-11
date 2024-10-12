000bb2cdfa1e568ce972153c3256a50b431e61a7
Hemit Patel <patelvhemit@gmail.com>
2024-10-12 18:56:48 -0400

    learned better

--- conditonal-control/LEARN BETTER (RANGES).py
+++ conditonal-control/LEARN BETTER (RANGES).py
@@ -7,15 +7,19 @@
 
 waves = int(input("Enter the amount of waves:"))
 
+
 if (waves > 6):
     print("Great day for surfing")
-    
-elif (waves >= 3 and waves < 6):
-    print("Go body boarding!")
 
-elif (waves >= 0 and waves < 3):
+# If waves are not above 6 feet, then we must check for ranges
+
+# GOING IN ASC order because its much easier to read!
+elif (0 <= waves <= 3):
     print("Go for a swim")
 
+elif (3 <= waves <= 6):
+    print("Go body boarding!")
 
+# If waves in feet don't make it to any of these ranges, output the following
 else:
     print("Whoa! What kind of a surf is that?")
