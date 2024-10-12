d5dfb73f8444a3a31202fb2c3c713b26e46d60cc
Hemit Patel <patelvhemit@gmail.com>
2024-10-12 19:06:19 -0400

    Update LEARN BETTER (RANGES).py

--- conditonal-control/LEARN BETTER (RANGES).py
+++ conditonal-control/LEARN BETTER (RANGES).py
@@ -14,6 +14,8 @@ if (waves > 6):
 # If waves are not above 6 feet, then we must check for ranges
 
 # GOING IN ASC order because its much easier to read!
+
+# We assume that the boundries (ranges) are exlusive unless explicity stated otherwise (since we are using mathemitcal logic
 elif (3 <= wave_height < 6):
         print("Go body boarding!")
 elif (0 <= wave_height < 3):
