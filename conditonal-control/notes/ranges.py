fc4ec2e7a2cf106165ae2816e01db10a16eb0492
Hemit Patel <patelvhemit@gmail.com>
2024-10-12 17:04:34 -0400

    Create ranges.py

---
+++ conditonal-control/notes/ranges.py
@@ -0,0 +1,22 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4 (re-orged)
+# RANGES NOTE
+# MR VEERA
+# 12 oct 2024
+
+# IN ORDER TO CREATE A RANGE USE THIS SYNTAX
+
+age = 4
+
+if (0 <= age <= 4):
+  print("AGE RANGE IS BETWEEN 0 and 4");
+
+elif (5 <= age <= 10):
+  print("AGE RANGE IS BETWEEN 5 and 10");
+
+
+# No = so it is NOT inclusive range
+
+elif (6 < age < 12):
+  print("AGE RANGE IS BETWEEN 7 and 11 because 6 != and 12 != ");
