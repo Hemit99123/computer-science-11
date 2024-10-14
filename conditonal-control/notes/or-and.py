caabb6dc76f97f791e7848b679292ed618b82ff7
Hemit Patel <patelvhemit@gmail.com>
2024-10-14 15:36:09 -0400

    Create or-and.py

---
+++ conditonal-control/notes/or-and.py
@@ -0,0 +1,22 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4 
+# NOTES FOR OR/AND OPERATORS
+# MR VEERA
+# 14 oct 2024
+
+
+# THESE OPERATORS ARE USED IF A PROGRAM REQUIRES MORE THAN ONE CONDITON FOR AN IF STATEMENT
+
+# OR -> used if ONLY one condition of the two needs to be true
+
+age = 50
+name = "Dwight"
+
+if (age < 50 or age > 100);
+  print("You are lower than 50 or above 100 in age")
+
+# AND -> used when both conditions need to be true
+
+if (age < 50 and name == "Dwight"):
+  print("You are 50 and your name is Dwight")
