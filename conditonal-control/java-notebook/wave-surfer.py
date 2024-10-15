00000416250a366b391c5ce96de39706d8cc67c7
Hemit Patel <patelvhemit@gmail.com>
2024-10-15 11:18:40 -0400

    Update wave-surfer.py

--- conditonal-control/java-notebook/wave-surfer.py
+++ conditonal-control/java-notebook/wave-surfer.py
@@ -12,10 +12,12 @@ wave_height = int(input("Enter the amount of wave height:"))
 
 if (wave_height >= 6):
     print("Great day for surfing!")
+# The wave must be BETWEEN 3 and 6 feet which means that 3 and 6 are not included 
 
 elif (3 < wave_height < 6):
     print("Go body boarding!")
 
+# It was worded from 0 to 3, the from means that the limits are counted
 elif (0 <= wave_height <= 3):
     print("Go swimming")
 
