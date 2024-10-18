73054b6f2f731e5df70cd53cbe16e3b0a44230a8
Hemit Patel <patelvhemit@gmail.com>
2024-10-18 17:33:06 -0400

    Update ex11.py

--- conditonal-control/java-notebook/ex11.py
+++ conditonal-control/java-notebook/ex11.py
@@ -1,28 +1,31 @@
 # Hemit Patel
-# 781159
-# ICS3U0-4 (reorged)
-# Q11 
 # MR VEERA
-# 8 oct 2024
+# ICS3UO-4
+# Ex 11 - Quadratic Equation
+# 781159
+# 18 oct 2024
 
-from math import sqrt
-a = int(input("Enter the value of a:"))
-b= int(input("Enter the value of b:"))
-c = int(input("Enter the value of c:"))
+import math
 
-# First find the root from the equation
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
 
-root = sqrt(b**2 - 4*a*c)
+# First find the discriminant
+discriminant = b**2 - 4*a*c
 
-# Then find the two solutions
-# -1 * b is to make the equation negative
-b_value = -1*b
+# Check if discriminant is non-negative (meaning there are 2 solutions)
+# and therefore can be used in this equation as this equation produces
+# 2 REAL numbers
 
-# Need 2 solutions since quadartic equation has 2 roots (one from adding)
-# one from subtracting
-
-
-root_solution_1 = (b_value + root) / (2*a)
-root_soultion_2 = (b_value - root) / (2*a)
-
-print(f'The roots are {root_solution_1} and {root_soultion_2}')
+if discriminant >= 0:
+    # If non-negative, find the square root
+    root = math.sqrt(discriminant)
+    
+    # Find the two solutions
+    root_solution_1 = (-b + root) / (2*a)
+    root_solution_2 = (-b - root) / (2*a)
+    
+    print(f'The roots are {root_solution_1:.1f} and {root_solution_2:.1f}')
+else:
+    print("This equation has complex roots.")
