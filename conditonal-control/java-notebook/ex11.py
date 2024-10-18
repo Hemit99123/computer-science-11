0cb46a972f54271b3c6baa1eb57c486574c1baad
Hemit Patel <patelvhemit@gmail.com>
2024-10-18 11:12:47 -0400

    Create ex11.py

---
+++ conditonal-control/java-notebook/ex11.py
@@ -0,0 +1,28 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4 (reorged)
+# Q11 
+# MR VEERA
+# 8 oct 2024
+
+from math import sqrt
+a = int(input())
+b= int(input())
+c = int(input())
+
+# First find the root from the equation
+
+root = sqrt(b**2 - 4*a*c)
+
+# Then find the two solutions
+# -1 * b is to make the equation negative
+b_value = -1*b
+
+# Need 2 solutions since quadartic equation has 2 roots (one from adding)
+# one from subtracting
+
+
+root_solution_1 = (b_value + root) / (2*a)
+root_soultion_2 = (b_value - root) / (2*a)
+
+print(f'The roots are {root_solution_1} and {root_soultion_2}')
