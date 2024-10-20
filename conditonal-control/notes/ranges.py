5e1dbb74ba6296ba15bb11b90016c8496677ce25
Hemit Patel <patelvhemit@gmail.com>
2024-10-20 14:50:44 -0400

    Update ranges.py

--- conditonal-control/notes/ranges.py
+++ conditonal-control/notes/ranges.py
@@ -9,14 +9,18 @@
 
 age = 4
 
+# from 0 to 4
 if (0 <= age <= 4):
   print("AGE RANGE IS BETWEEN 0 and 4");
 
+# from 5 to 10
 elif (5 <= age <= 10):
   print("AGE RANGE IS BETWEEN 5 and 10");
 
 
 # No = so it is NOT inclusive range
+
+# BETWEEN 6 and 12 (non-inclusive = between)
 
 elif (6 < age < 12):
   print("AGE RANGE IS BETWEEN 7 and 11 because 6 != and 12 != ");
