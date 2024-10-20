5f6752988f780eb811fceeae07733eb41bb9ec07
Hemit Patel <patelvhemit@gmail.com>
2024-10-20 15:15:20 -0400

    Create importing_rules.py

---
+++ conditonal-control/notes/importing_rules.py
@@ -0,0 +1,16 @@
+# Hemit Patel
+# MR VEERA 
+# ICS3U0-4
+# 20 oct 2024
+# Importing Notes Rules
+
+# use this syntax so you don't import the whole lib but only the functions that you will be using
+
+from math import pi, e, log, sin, cos
+
+# use "as" which will change the name of the function to something else to make it more descriptive
+# for example...
+
+# this changes the function e which is not that descriptive to eular which makes more sense
+
+from math import e as eular
