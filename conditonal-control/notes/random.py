7c814cf0b78eea3ba5b2a61436339fb390d93c4a
Hemit Patel <patelvhemit@gmail.com>
2024-10-20 14:54:32 -0400

    Create random.py

---
+++ conditonal-control/notes/random.py
@@ -0,0 +1,9 @@
+
+# only need one function so just import that not the whole lib (saving computation costs)
+
+from random import randint
+
+# the upper and lower limits are inclusive, meaning they could be producec
+
+RANDOM_NUMBER = randint(1,10)
+
