3e19257feac0ea54e1a85f67faf28f54f80ab97f
Hemit Patel <patelvhemit@gmail.com>
2024-10-20 15:17:38 -0400

    Update math.py

--- conditonal-control/notes/math.py
+++ conditonal-control/notes/math.py
@@ -3,43 +3,43 @@
 # Mr Veera
 # 17 Oct 2024
 
-import math
+from math import sin, tan, cos, atan, acos, asin, pi, e as euler, degrees, radians, log
 
 angle=float(input("Give an angle: ")) 
 
 # Converts angle from deg to radians   
-angle_rad=math.radians(angle)
+angle_rad= radians(angle)
                                       
 
 # gets sine from radian 
-sine_value=math.sin(angle_rad)
+sine_value= sin(angle_rad)
 
 # gets tan 
-tan_value = math.tan(angle_rad) 
+tan_value = tan(angle_rad) 
 
 # gets cops
-cos_value = math.cos(angle_rad) 
+cos_value = cos(angle_rad) 
 
 # rounded value
 print(" Sine of {:.2f} Deg = {:.2f}".format(angle,sine_value))
 
 # asin, atan and acos find the original angle theta based on the sin,cos,tan value
 
-original_angle_rad = math.asin(sine_value)
-original_angle_rad = math.acos(tan_value)
-original_angle_rad = math.atan(cos_value)
+original_angle_rad = asin(sine_value)
+original_angle_rad = acos(tan_value)
+original_angle_rad = atan(cos_value)
 
 # converts from radians to deg
-original_angle_deg=math.degrees(original_angle_rad) 
+original_angle_deg= degrees(original_angle_rad) 
 
 # in trig, go to decimal places up to 4
 print('Given angle is {:.4f} rad'.format(original_angle_rad))
 print('Given angle is {:.2f} deg'.format(original_angle_deg))
 
-print(math.pi) # gets pi constant
-print(math.e) # gets euler constant
+print(pi) # gets pi constant
+print(euler) # gets euler constant
 
-logarithm_value = math.log(1000) # calculates logarithms
+logarithm_value = log(1000) # calculates logarithms
 print(logarithm_value)
 
 
