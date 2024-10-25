96227de46095cfab620e611490967dd67261a8ef
Hemit Patel <patelvhemit@gmail.com>
2024-10-25 07:51:13 -0400

    Create treasure.py

---
+++ conditonal-control/extra/treasure.py
@@ -0,0 +1,18 @@
+# Hemit Patel
+# MR VEERA
+# ICS3U0-4
+# 781159
+# oct 25 2024
+# treasure problem
+# MR VEERA
+
+treasure = int(input("Enter the gold coins:"))
+
+
+# since sharing 2 ways, we see if it is be split by 2
+# evenally
+
+if (treasure % 2 == 0):
+    print("yes")
+else:
+    print("no")
