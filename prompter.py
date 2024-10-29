65416f38e0f2e0c5d9fdaa12ddf67c026e557bed
Hemit Patel <patelvhemit@gmail.com>
2024-10-29 11:18:21 -0400

    Create prompter.py

---
+++ prompter.py
@@ -0,0 +1,18 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# MR VEERA
+# Prompter Java Notebook
+
+minimum = int(input())
+maximum = int(input())
+number = int(input())
+
+# this is the opposite of the range boundary so that
+# it qwill check if number is OUTSIDE the range
+
+while (minimum > number > maximum):
+    number = int(input(f"Give a number between {minimum} and {maximum}"))
+
+    
+
