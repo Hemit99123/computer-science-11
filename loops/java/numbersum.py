7863193060ec80c059daed95cf7a0909f2633af9
Hemit Patel <patelvhemit@gmail.com>
2024-10-30 11:16:22 -0400

    Create numbersum.py

---
+++ loops/java/numbersum.py
@@ -0,0 +1,20 @@
+# Hemit Patel
+# 781159
+# MR VEERA
+# ICS3U0-4
+# 29 oct 2024
+
+total = 0 # creating a variable to store the total value
+
+number = int(input("Enter a number:")) # Complete the prompt in this line
+counter = 1
+
+# asc
+while (counter <= number):
+    print(counter)
+    counter += 1
+    total+=number
+
+print("The total is", total)
+
+
