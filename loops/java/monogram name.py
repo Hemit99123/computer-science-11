de87e479844ef6fca39e57362257b2e03b9d9ab0
Hemit Patel <patelvhemit@gmail.com>
2024-11-17 14:44:02 -0500

    Create monogram name.py

---
+++ loops/java/monogram name.py
@@ -0,0 +1,15 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# MR VEERA
+# Monogram
+# 17 nov 2024
+
+
+first_name = input("Enter your first name:")
+middle_initial = input("Enter your middle initial:")
+last_name = input("Enter your last name:")
+
+monogram = first_name[0].lower() + last_name[0].upper() + middle_initial.lower()
+
+print("Your monogram is", monogram)
