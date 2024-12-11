8e89db17f85c58ec46c26d19d57ebe5df0ae4460
Hemit Patel <hemitvpatel@gmail.com>
2024-12-11 10:32:57 -0500

    Create palindrome.py

---
+++ lists/palindrome.py
@@ -0,0 +1,12 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4 
+# 11 dec 2024 
+# Palindrome
+
+word = input("Enter a word:")
+
+if (word[::-1] == word):
+    print("Palindrome")
+else:
+    print("Is not palindrome")
