6b855a8f43f8c328d9957392bca1c53c470711e9
Hemit Patel <hemitpatel@Hemits-MacBook-Air.local>
2024-12-10 22:20:48 -0500

    hw

---
+++ lists/Analysis.py
@@ -0,0 +1,13 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4 
+# 10 dec 2024 
+# Analysis (numbers)
+
+
+while (True):
+    num = int(input("Enter your number:"))
+
+    # we are making the number 0 the sential
+    if (num == 0):
+        break
