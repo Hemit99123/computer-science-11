41e05d78036c6fc0aaca6c34b1b198a5bd6a44c6
Hemit Patel <hemitvpatel@gmail.com>
2024-12-12 11:00:01 -0500

    Create CODER APPLICATION.py

---
+++ lists/CODER APPLICATION.py
@@ -0,0 +1,25 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# MR VEERA
+# 12 dec 2024
+# coder application
+
+word = input("Enter a string:").lower()
+coded = ""
+
+# no need for list, strings also have indexes and better for memory
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+for letter in word:
+    
+    if (letter not in alphabet):
+        coded += letter
+        continue
+    
+    index = alphabet.index(letter)
+
+    coded += alphabet[index+2]
+
+
+print(coded)
