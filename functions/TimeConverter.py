fc28548d41f39db46cc2200bc52177daab1d5891
Hemit Patel <hemitvpatel@gmail.com>
2025-01-06 11:13:50 -0500

    added all defs

---
+++ functions/TimeConverter.py
@@ -0,0 +1,23 @@
+# Hemit Patel
+# 781159
+# ICS3U0
+# MR VEERA
+# Time Converter App
+# 6 jan 2025
+
+def hours_to_minutes(hours):
+    minutes = hours * 60
+    return minutes
+
+def days_to_hours(days):
+    hours = days * 24
+    return hours
+
+def minutes_to_hours(minutes):
+    hours = minutes / 60
+    return hours
+
+def hours_to_days(hours):
+    days = hours / 24
+    return days
+
