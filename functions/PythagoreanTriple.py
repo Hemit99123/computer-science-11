317c7077fc4d3d2a5e9db778a1481b0548ad0ce2
Hemit Patel <hemitvpatel@gmail.com>
2025-01-09 16:08:05 +0000

    added pyhtagorean triple

--- functions/PythagoreanTriple.py
+++ functions/PythagoreanTriple.py
@@ -2,5 +2,28 @@
 # 781159
 # ICS3U0-4 
 # MR VEERA
-# is prime
+# pythagorean triple
 # 9 jan 2025
+
+def is_perfect_square(n):
+    root = int(n ** 0.5)
+    return root * root == n
+
+def find_pythagorean_triples():
+    triples = []
+    for a in range(1, 100):
+        # for each "a" variable, we need to check every combination
+        # it can have with a "b" variable
+        
+        # this is why we start the range at a because a itself should not be included
+        
+        for b in range(a, 100):  
+            c_squared = a*a + b*b
+            if is_perfect_square(c_squared):
+                c = int(c_squared ** 0.5)
+                triples.append((a, b, c))
+    return triples
+
+res = " ,".join(map(str, find_pythagorean_triples(a,b)))
+
+print(res)
