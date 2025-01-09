7c980c9f1e6986925786fc5e5f3dcf4868f9070d
Hemit Patel <hemitvpatel@gmail.com>
2025-01-09 10:12:16 -0500

    Create distance conversion.py

---
+++ functions/distance conversion.py
@@ -0,0 +1,44 @@
+def inches_to_cm(inches):
+    """Convert inches to centimeters"""
+    return inches * 2.54
+
+def cm_to_inches(cm):
+    """Convert centimeters to inches"""
+    return cm / 2.54
+
+def feet_to_cm(feet):
+    """Convert feet to centimeters"""
+    return feet * 30
+
+def cm_to_feet(cm):
+    """Convert centimeters to feet"""
+    return cm / 30
+
+def yards_to_meters(yards):
+    """Convert yards to meters"""
+    return yards * 0.91
+
+def meters_to_yards(meters):
+    """Convert meters to yards"""
+    return meters / 0.91
+
+def miles_to_km(miles):
+    """Convert miles to kilometers"""
+    return miles * 1.6
+
+def km_to_miles(km):
+    """Convert kilometers to miles"""
+    return km / 1.6
+
+def display_menu():
+    """Display the conversion menu"""
+    print("\nMetric Conversion Calculator")
+    print("1. Inches to Centimeters")
+    print("2. Centimeters to Inches")
+    print("3. Feet to Centimeters")
+    print("4. Centimeters to Feet")
+    print("5. Yards to Meters")
+    print("6. Meters to Yards")
+    print("7. Miles to Kilometers")
+    print("8. Kilometers to Miles")
+    print("9. Exit")
