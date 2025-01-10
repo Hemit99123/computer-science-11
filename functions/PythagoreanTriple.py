ac4e4fecc3b2247e30bda7dd9e0c6544502aeb55
Hemit Patel <hemitvpatel@gmail.com>
2025-01-10 15:26:18 +0000

    updated sum things

--- functions/PythagoreanTriple.py
+++ functions/PythagoreanTriple.py
@@ -5,9 +5,9 @@
 # pythagorean triple
 # 9 jan 2025
 
-def is_perfect_square(n):
-    root = int(n ** 0.5)
-    return root * root == n
+def is_perfect_square(number):
+    root = int(number ** 0.5)
+    return root * root == number
 
 def find_pythagorean_triples():
     triples = []
@@ -21,6 +21,7 @@ def find_pythagorean_triples():
             c_squared = a*a + b*b
             if is_perfect_square(c_squared):
                 c = int(c_squared ** 0.5)
+                # adding as a tuple so these values are immutable 
                 triples.append((a, b, c))
     return triples
 
---
+++ functions/distance_conversion.py
@@ -0,0 +1,117 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4 
+# MR VEERA
+# distance conversion
+# 9 jan 2025
+
+def inches_to_centimeters(inches):
+    """Convert inches to centimeters"""
+    return inches * 2.54
+
+def cm_to_inches(cm):
+    """Convert centimeters to inches"""
+    return cm / 2.54
+
+def feet_to_centimeters(feet):
+    """Convert feet to centimeters"""
+    return feet * 30
+
+def cm_to_feet(cm):
+    """Convert centimeters to feet"""
+    return cm / 30
+
+def yards_to_meters(yards):
+    """Convert yards to meters"""
+    return yards * 0.91
+
+def meters_to_yards(meters):
+    """Convert meters to yards"""
+    return meters / 0.91
+
+def miles_to_kilometers(miles):
+    """Convert miles to kilometers"""
+    return miles * 1.6
+
+def km_to_miles(km):
+    """Convert kilometers to miles"""
+    return km / 1.6
+
+def display_menu():
+    """Display the conversion menu"""
+    print("\nMetric Conversion Calculator")
+    print("1. Inches to Centimeters")
+    print("2. Centimeters to Inches")
+    print("3. Feet to Centimeters")
+    print("4. Centimeters to Feet")
+    print("5. Yards to Meters")
+    print("6. Meters to Yards")
+    print("7. Miles to Kilometers")
+    print("8. Kilometers to Miles")
+    print("9. Exit")
+
+display_menu()
+
+type_of_conversion = int(input("What type?"))
+number = int(input("Amount:"))
+
+if (type_of_conversion == 1):
+
+    centimeters = inches_to_centimeters(number)
+
+    print (number, "inches equals", centimeters, "centimeters")
+
+
+elif (type_of_conversion == 2):
+
+    centimeters = feet_to_centimeters (number)
+
+    print (number, "feet equals", centimeters, "centimeters")
+
+
+elif (type_of_conversion == 3):
+
+    meters = yards_to_meters (number)
+
+    print (number, "yards equals", meters, "meters")
+
+
+elif (type_of_conversion == 4):
+
+    kilometers = miles_to_kilometers (number)
+
+    print (number, "miles equals", kilometers, "kilometers")
+
+    
+
+elif (type_of_conversion == 5):
+
+    inches = centimeters_to_inches (number)
+
+    print (number, "centimeters equals", inches, "inches")
+
+
+elif (type_of_conversion== 6):
+
+    feet = centimeters_to_feet (number)
+
+    print (number, "centimeters equals", feet, "feet")
+
+
+elif (type_of_conversion == 7):
+
+    yards = meters_to_yards (number)
+
+    print (number, "meters equals", yards, "yards")
+
+
+elif (type_of_conversion == 8):
+
+    miles = kilometers_to_miles(number)
+
+    print (number, "kilometers equals", miles, "miles")
+
+
+else:
+
+    print ("null")
